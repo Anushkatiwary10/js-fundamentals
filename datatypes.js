55d1bc693eb9255176b4aee1d4b5217ba1fb66fb
Anushka Tiwary <tiwaryanushka9@gmail.com>
2026-06-06 17:40:20 +0000

    changes

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,8 @@
-console.log("anushka");
+//console.log("anushka");
+let value=3;
+let negValue=-value
+//console.log(negValue)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
